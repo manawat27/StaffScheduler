3f34506f18072d28bda2f61d5fa4ac4aea94f8a4
manawat27 <66442887+manawat27@users.noreply.github.com>
2026-02-01 21:56:15 -0800

    Updated layout

--- frontend/src/Layouts/SideBar/SideBar.tsx
+++ frontend/src/Layouts/SideBar/SideBar.tsx
@@ -5,12 +5,26 @@ import GroupsIcon from "@mui/icons-material/Groups"
 import PersonIcon from "@mui/icons-material/Person"
 import DashboardIcon from "@mui/icons-material/Dashboard"
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth"
+import CorporateFareIcon from "@mui/icons-material/CorporateFare"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { UserCircleIcon } from "@hugeicons/core-free-icons"
+import LogoutIcon from "@mui/icons-material/Logout"
+import KeycloakService from "@/auth/keycloakService"
 
 export default function SideBar() {
+  const [isAdmin, setIsAdmin] = useState(false)
   const [collapsed, setCollapsed] = useState(() => {
     const stored = localStorage.getItem("sidebar-collapsed")
     return stored === "true"
   })
+
+  useEffect(() => {
+    const userRole = KeycloakService.getUserInfo()?.roles || []
+    if (userRole.includes("admin")) {
+      // set isAdmin to true if user has admin role
+      setIsAdmin(true)
+    }
+  }, [])
 
   useEffect(() => {
     localStorage.setItem("sidebar-collapsed", collapsed.toString())
@@ -29,9 +43,17 @@ export default function SideBar() {
         </a>
       </button>
       <div
-        className={`mb-8 text-2xl font-bold text-center tracking-wide ${collapsed ? "hidden" : ""}`}
+        className={`mb-8 text-2xl font-bold flex items-center justify-center ${collapsed ? "hidden" : ""}`}
       >
-        Menu
+        <HugeiconsIcon
+          icon={UserCircleIcon}
+          size={90}
+          color="currentColor"
+          strokeWidth={1.5}
+        />
+        <span className="ml-2">
+          Hi, {KeycloakService.getUserInfo()?.firstName}!
+        </span>
       </div>
       <NavLink
         to="/account"
@@ -73,6 +95,29 @@ export default function SideBar() {
         <CalendarMonthIcon className="mr-2" />
         {!collapsed && "Schedule"}
       </NavLink>
+
+      {isAdmin && (
+        <NavLink
+          to="/admin"
+          className={({ isActive }) =>
+            `py-3 px-4 rounded hover:bg-blue-700 transition mb-2 flex items-center ${collapsed ? "justify-center" : ""} ${isActive ? "bg-blue-700" : ""}`
+          }
+          title="Organization"
+        >
+          <CorporateFareIcon className="mr-2" />
+          {!collapsed && "Organization"}
+        </NavLink>
+      )}
+
+      <a
+        href="#"
+        className={`py-3 px-4 rounded hover:bg-blue-700 transition mb-2 flex items-center ${collapsed ? "justify-center" : ""}`}
+        onClick={() => KeycloakService.kcLogout()}
+        title="Logout"
+      >
+        <LogoutIcon className="mr-2" />
+        {!collapsed && "Logout"}
+      </a>
     </nav>
   )
 }
